39706896845b658e02ed3b96074cdd49743cf825
Janusz Sawicki <janusz_sawicki97@o2.pl>
2020-11-17 15:03:44 +0100

    sql

--- routes/login.js
+++ routes/login.js
@@ -26,7 +26,11 @@ module.exports = function login () {
 
   return (req, res, next) => {
     verifyPreLoginChallenges(req)
-    models.sequelize.query(`SELECT * FROM Users WHERE email = '${req.body.email || ''}' AND password = '${insecurity.hash(req.body.password || '')}' AND deletedAt IS NULL`, { model: models.User, plain: true })
+    email = `${req.body.email || ''}`;
+    models.sequelize.query(`SELECT * FROM Users WHERE email = ? AND password = '${insecurity.hash(req.body.password || '')}' AND deletedAt IS NULL`, { model: models.User, plain: true },
+    {
+      replacements: [criteria, criteria]
+    })
       .then((authenticatedUser) => {
         let user = utils.queryResultToJson(authenticatedUser)
         const rememberedEmail = insecurity.userEmailFrom(req)
